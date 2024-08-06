8446e2af6bf29b674ab1ebc7c464ec891d3e880a
yoonoh.jung <yoonoh.jung@linecorp.com>
2024-08-06 15:18:29 +0900

    Initial Commit

---
+++ WatchWithIosApp Watch App/WatchWithIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWithIosAppApp.swift
+//  WatchWithIosApp Watch App
+//
+//  Created by USER on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchWithIosApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchWithIosApp/WatchWithIosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWithIosAppApp.swift
+//  WatchWithIosApp
+//
+//  Created by USER on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchWithIosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
